5cbfc89b3b718260efd4b5b2023688c934b0f2cf
crist <cristhianchulca49@gmail.com>
2025-11-12 13:51:53 +0100

    Create objects of subclasses and call their play() methods

--- src/main/java/Level_1_E1/Main.java
+++ src/main/java/Level_1_E1/Main.java
@@ -2,6 +2,11 @@ package Level_1_E1;
 
 public class Main {
     public static void main(String[] args) {
-
+        Instrument flute = new WindInstrument("Yamaha", 500);
+        Instrument guitar = new StringInstrument("Gibson", 1680);
+        Instrument drums = new PercussionInstrument("Mapex", 500);
+        flute.play();
+        guitar.play();
+        drums.play();
     }
 }
