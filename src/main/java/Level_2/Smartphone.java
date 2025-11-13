cbb9ff4753308dc44ad669eb3f4303b13d3be08b
crist <cristhianchulca49@gmail.com>
2025-11-13 13:10:22 +0100

    feat: implement photograph() and alarm() methods in Smartphone class to print messages

--- src/main/java/Level_2/Smartphone.java
+++ src/main/java/Level_2/Smartphone.java
@@ -2,4 +2,13 @@ package Level_2;
 
 public class Smartphone extends Phone implements Camera, Watch{
 
+    @Override
+    public void photograph() {
+        System.out.println("A photo is begin take from Smartphone");
+    }
+
+    @Override
+    public void alarm() {
+        System.out.println("The alarm is ringing from Smartphone");
+    }
 }
