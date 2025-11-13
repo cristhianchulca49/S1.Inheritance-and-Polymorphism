7e4a1caf2d269345e3130552dafe12537421aa5f
crist <cristhianchulca49@gmail.com>
2025-11-13 12:32:05 +0100

    feat: update alarm() and photograph() methods to display messages

--- src/main/java/Level_2/Camera.java
+++ src/main/java/Level_2/Camera.java
@@ -2,6 +2,6 @@ package Level_2;
 
 public interface Camera {
     public static void photograph() {
-
+        System.out.println("A photo is begin take");
     }
 }
--- src/main/java/Level_2/Watch.java
+++ src/main/java/Level_2/Watch.java
@@ -2,6 +2,6 @@ package Level_2;
 
 public interface Watch {
     public static void alarm() {
-
+        System.out.println("The alarm is ringing");
     }
 }
