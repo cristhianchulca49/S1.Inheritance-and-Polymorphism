b3311e9c7dae6f10200388256583a22babfbfdf7
crist <cristhianchulca49@gmail.com>
2025-11-13 10:59:54 +0100

    Add new Car instance and call static and instance methods

--- src/main/java/Level_1_E2/Main.java
+++ src/main/java/Level_1_E2/Main.java
@@ -2,6 +2,8 @@ package Level_1_E2;
 
 public class Main {
     public static void main(String[] args) {
-
+        Car merdecez = new Car(3500);
+        Car.brake();
+        merdecez.accelerate();
     }
 }
