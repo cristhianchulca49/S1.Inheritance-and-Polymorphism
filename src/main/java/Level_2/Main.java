0e887607d65a2344867e879e216dde8e13521288
crist <cristhianchulca49@gmail.com>
2025-11-13 13:15:05 +0100

    feat: create Smartphone object and call call(), alarm() and photograph() methods

--- src/main/java/Level_2/Main.java
+++ src/main/java/Level_2/Main.java
@@ -2,6 +2,9 @@ package Level_2;
 
 public class Main {
     public static void main(String[] args) {
-
+        Smartphone samsung = new Smartphone();
+        samsung.call("670398665");
+        samsung.alarm();
+        samsung.photograph();
     }
 }
