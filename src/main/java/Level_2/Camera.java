8f8735d660c671bd486a1dda2ad014f982ee5f19
crist <cristhianchulca49@gmail.com>
2025-11-13 13:06:56 +0100

    fix: correct photograph() and alarm() method to non-static so subclasses must implement it

--- src/main/java/Level_2/Camera.java
+++ src/main/java/Level_2/Camera.java
@@ -1,7 +1,5 @@
 package Level_2;
 
 public interface Camera {
-    public static void photograph() {
-        System.out.println("A photo is begin take");
-    }
+    void photograph();
 }
--- src/main/java/Level_2/Watch.java
+++ src/main/java/Level_2/Watch.java
@@ -1,7 +1,5 @@
 package Level_2;
 
 public interface Watch {
-    public static void alarm() {
-        System.out.println("The alarm is ringing");
-    }
+    void alarm();
 }
