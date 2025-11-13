f2685ba2b196351dda683284e726e551ce7696fb
crist <cristhianchulca49@gmail.com>
2025-11-13 11:25:03 +0100

    feat: update call() method to accept a phone number

--- src/main/java/Level_2/Phone.java
+++ src/main/java/Level_2/Phone.java
@@ -4,6 +4,7 @@ public class Phone {
     private String brand;
     private String model;
 
-    public void call() {
+    public void call(String number) {
+        System.out.println("Calling to: " + number);
     }
 }
