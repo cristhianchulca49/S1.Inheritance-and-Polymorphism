83d4ca6c982a98a5ea4f8e9fa659174f1348ae7a
crist <cristhianchulca49@gmail.com>
2025-11-13 10:25:44 +0100

    Define brand as static final, model as static, and power as final in Car class

--- src/main/java/Level_1_E2/Car.java
+++ src/main/java/Level_1_E2/Car.java
@@ -1,7 +1,7 @@
 package Level_1_E2;
 
 public class Car {
-    private String brand = "Mercedes Benz";
-    private String model;
-    private int power;
+    private static final String brand = "Mercedes Benz";
+    private static String model;
+    private final int power = 3500;
 }
