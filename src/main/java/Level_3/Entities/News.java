20db8bf894a56b6f7656b27fd4a43cdc4ed267e2
cristhianchulca49 <cristhianchulca49@gmail.com>
2025-12-26 23:36:48 +0100

    refactor: rename class to english language

--- src/main/java/Level_3/Entities/News.java
+++ src/main/java/Level_3/Entities/News.java
@@ -2,13 +2,13 @@ package Level_3.Entities;
 
 import java.math.BigDecimal;
 
-public abstract class Notice {
+public abstract class News {
     private String title;
     private String description;
     private int score;
     private BigDecimal price;
 
-    public Notice(String title, int score, BigDecimal price) {
+    public News(String title, int score, BigDecimal price) {
         this.title = title;
         this.score = score;
         this.price = price;
