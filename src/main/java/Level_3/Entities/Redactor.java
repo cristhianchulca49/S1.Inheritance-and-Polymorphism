ed8433e88fe2d1e9b4ec8a3467924ad4969d37af
cristhianchulca49 <cristhianchulca49@gmail.com>
2025-12-26 23:30:14 +0100

    feat: add attributes, constructor, getters y setters

--- src/main/java/Level_3/Entities/Redactor.java
+++ src/main/java/Level_3/Entities/Redactor.java
@@ -1,4 +1,26 @@
 package Level_3.Entities;
 
+import java.math.BigDecimal;
+
 public class Redactor {
+    private final String DNI;
+    private String name;
+    static private final BigDecimal salary = BigDecimal.valueOf(1500);
+
+    public Redactor(String DNI, String name) {
+        this.DNI = DNI;
+        this.name = name;
+    }
+
+    public String getDNI() {
+        return DNI;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public BigDecimal getSalary() {
+        return salary;
+    }
 }
