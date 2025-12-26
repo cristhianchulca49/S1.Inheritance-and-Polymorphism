ad2be53f35a61bb6f781a1b0d6222580b611a645
cristhianchulca49 <cristhianchulca49@gmail.com>
2025-12-26 23:31:08 +0100

    feat: add attributes, constructor, getters y setters

--- src/main/java/Level_3/Entities/Notice.java
+++ src/main/java/Level_3/Entities/Notice.java
@@ -1,4 +1,16 @@
 package Level_3.Entities;
 
-public class Notice {
+import java.math.BigDecimal;
+
+public abstract class Notice {
+    private String title;
+    private String description;
+    private int score;
+    private BigDecimal price;
+
+    public Notice(String title, int score, BigDecimal price) {
+        this.title = title;
+        this.score = score;
+        this.price = price;
+    }
 }
