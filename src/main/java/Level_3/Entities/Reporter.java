c23a5167546d1338d044e9436e37ba24dce47c28
cristhianchulca49 <cristhianchulca49@gmail.com>
2025-12-29 17:40:09 +0100

    feat: add news getters and setter and new List attribute

---
+++ src/main/java/Level_3/Entities/Reporter.java
@@ -0,0 +1,37 @@
+package Level_3.Entities;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Reporter {
+    private final String DNI;
+    private String name;
+    private static BigDecimal salary = BigDecimal.valueOf(1500);
+    private List<News> reports = new ArrayList<>();
+
+    public Reporter(String DNI, String name) {
+        this.DNI = DNI;
+        this.name = name;
+    }
+
+    public String getDNI() {
+        return DNI;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public BigDecimal getSalary() {
+        return salary;
+    }
+
+    public static void setSalary(BigDecimal newSalary) {
+        Reporter.salary = newSalary;
+    }
+
+    public List<News> getNews() {
+        return reports;
+    }
+}
