29dccdcb2da1e1dc7ee4315b962ed7f50c1a944e
cristhianchulca49 <cristhianchulca49@gmail.com>
2025-12-29 17:56:31 +0100

    feat: add news getters and setter and change access modifier

--- src/main/java/Level_3/Entities/News.java
+++ src/main/java/Level_3/Entities/News.java
@@ -1,16 +1,34 @@
 package Level_3.Entities;
+
 import java.math.BigDecimal;
 
 public abstract class News {
     private String title;
     private String description;
-    private int score;
-    private BigDecimal price;
+    protected int score;
+    protected BigDecimal price;
 
     public News(String title) {
         this.title = title;
     }
 
+    public String getTitle() {
+        return title;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
     public abstract void calculatePrice();
+
     public abstract void calculateScore();
 }
