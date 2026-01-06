59a69fc50841408a78542f695b3fcbfd83f763b8
cristhianchulca49 <cristhianchulca49@gmail.com>
2026-01-06 16:17:50 +0100

    feat: add player2 attribute

--- src/main/java/Level_3/Entities/Tennis.java
+++ src/main/java/Level_3/Entities/Tennis.java
@@ -2,16 +2,16 @@ package Level_3.Entities;
 
 import Level_3.Service.TennisNewsService;
 
-import java.math.BigDecimal;
-
 public class Tennis extends News {
     private final String competition;
     private final String player;
+    private final String player2;
 
-    public Tennis(String title, String competition, String player) {
+    public Tennis(String title, String competition, String player, String player2) {
         super(title);
         this.competition = competition;
         this.player = player;
+        this.player2 = player2;
 
     }
 
@@ -20,7 +20,7 @@ public class Tennis extends News {
         this.price = TennisNewsService.getBasePrice();
 
         TennisNewsService.getPriceRules().forEach((ruleKey, extraPrice) -> {
-            if (ruleKey.equalsIgnoreCase(competition) || ruleKey.equalsIgnoreCase(player)) {
+            if (ruleKey.equalsIgnoreCase(competition) || ruleKey.equalsIgnoreCase(player) || ruleKey.equalsIgnoreCase(player2)) {
                 this.price = price.add(extraPrice);
             }
         });
@@ -32,7 +32,7 @@ public class Tennis extends News {
         this.score = TennisNewsService.getBaseScore();
 
         TennisNewsService.getScoreRules().forEach((ruleKey, extraScore) -> {
-            if (ruleKey.equalsIgnoreCase(competition) || ruleKey.equalsIgnoreCase(player)) {
+            if (ruleKey.equalsIgnoreCase(competition) || ruleKey.equalsIgnoreCase(player) || ruleKey.equalsIgnoreCase(player2)) {
                 this.score = this.score + extraScore;
             }
         });
