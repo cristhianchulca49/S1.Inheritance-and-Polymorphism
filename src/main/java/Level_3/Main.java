b6bdceb80dfd1614a5a8c8ba7c99568d23328f5a
cristhianchulca49 <cristhianchulca49@gmail.com>
2026-01-07 21:13:42 +0100

    refactor: add simple menu with create and delete reporter

--- src/main/java/Level_3/Main.java
+++ src/main/java/Level_3/Main.java
@@ -1,4 +1,31 @@
 package Level_3;
 
+import Level_3.UI.MenuUI;
+
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        MenuUI menuUI = new MenuUI();
+        System.out.println("Sports News Writing");
+
+        while (true) {
+            System.out.println("What do you want to do now?:");
+            switch (sc.nextLine()) {
+                case "1":
+                    menuUI.createReporter();
+                    break;
+                case "2":
+                    menuUI.deleteReporter();
+                case "3":
+                    break;
+                case "0":
+                    return;
+            }
+
+        }
+
+
+    }
 }
