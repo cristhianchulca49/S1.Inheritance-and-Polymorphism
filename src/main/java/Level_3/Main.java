ebbe71a884d155375a62547167ca84825be4615c
cristhianchulca49 <cristhianchulca49@gmail.com>
2026-01-08 15:26:58 +0100

    feat: add List<String> with options of menu

--- src/main/java/Level_3/Main.java
+++ src/main/java/Level_3/Main.java
@@ -3,12 +3,26 @@ package Level_3;
 import Level_3.UI.Input;
 import Level_3.UI.MenuUI;
 
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         MenuUI menuUI = new MenuUI();
+        List<String> menu = List.of(
+                "1: Create Reporter",
+                "2: Delete Reporter",
+                "3: Add news to Reporter",
+                "4: Delete News",
+                "5: Show News from a Reporter",
+                "6: Calculate the score of the news",
+                "7: Calculate the price of the news",
+                "0: Exit"
+        );
+
         System.out.println("Sports News Writing");
 
         while (true) {
+            menu.forEach(System.out::println);
             char option = Input.readChar("What do you want to do now?:");
             switch (option) {
                 case '1':
